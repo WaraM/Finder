7e2152a27e4e4f0dbbf113cbb576114a8bd6674a
WaraM <maxime.cougourdan@laposte.net>
2018-03-19 21:24:06 +0100

    Testing stuff for web client

--- app.js
+++ app.js
@@ -59,11 +59,11 @@ app.use(expressValidator({
     }
 }));
 
-//
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.headers.add('Access-Control-Allow-Credentials', 'true')
   next();
 });
 
