759e6e8e938e4da638ecab985fc701632b7b5bbb
WaraM <maxime.cougourdan@laposte.net>
2018-03-19 21:34:06 +0100

    oups

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.headers.add('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
